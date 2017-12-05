a688ee566705f08b319f43d94067ef0da0bf8306
Grant Wallace <gwallace@princeton.edu>
2017-12-05 11:15:54 -0500

    rename FindNewestFile.py to utils.py in preparation for other utility functions

--- rtAttenPy/__init__.py
+++ rtAttenPy/__init__.py
@@ -1,9 +1,11 @@
-from .FindNewestFile import findNewestFile
+from .utils import findNewestFile
 from .highpass import highpass, highpass_opt, hp_convkernel
 from .smooth import smooth
+#from .RealTimePunisherFileProcess import realTimePunisherProcess
 
 __all__ = [
     'findNewestFile',
+    #'realTimePunisherProcess',
 
     'highpass',
     'highpass_opt',
