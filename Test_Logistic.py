5d52dc1ffda1a57af3c5b1f2c329e4426c14dffe
Daniel Suo <danielsuo@gmail.com>
2017-12-06 23:35:34 -0500

    Update logistic regression

--- Test_Logistic.py
+++ Test_Logistic.py
@@ -6,7 +6,8 @@ a = scipy.io.loadmat('logistic_in.mat')
 b = scipy.io.loadmat('logistic_out.mat')
 
 data = a['a']['data'][0][0]
-labels = [x[0] fpr x in a['a']['labels'][0][0]]
+labels = [x[0] if x[0] == 1 else -1 for x in a['a']['labels'][0][0]]
+print(labels)
 
 classifier = rtAttenPy.logistic(data, labels)
 
