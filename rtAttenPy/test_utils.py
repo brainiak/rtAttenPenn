75d17728fe4f33d14c07440794159ed0870e91dc
Grant Wallace <gwallace@princeton.edu>
2017-12-08 11:06:27 -0500

    modifications to MatlabStructDict to allow setting subfield variables

--- rtAttenPy/test_utils.py
+++ rtAttenPy/test_utils.py
@@ -28,28 +28,35 @@ class TestFindNewestFile(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def assert_result_matches_filename(self, filename):
+        self.assertEqual(filename, TEST_BASE_FILENAME + str(NUM_TEST_FILES-1))
+
     def test_normalCase(self):
+        print("Test findNewestFile normal case:")
         filename = utils.findNewestFile('/tmp/testdir', 'file1_20170101*')
         self.assert_result_matches_filename(filename)
 
     def test_emptyPath(self):
+        print("Test findNewestFile empty path:")
         filename = utils.findNewestFile('', '/tmp/testdir/file1_20170101*')
         self.assert_result_matches_filename(filename)
 
     def test_pathInPattern(self):
+        print("Test findNewestFile path embedded in pattern:")
         filename = utils.findNewestFile('/tmp/testdir', '/tmp/testdir/file1_20170101*')
         self.assert_result_matches_filename(filename)
 
     def test_pathPartiallyInPattern(self):
+        print("Test findNewestFile path partially in pattern:")
         filename = utils.findNewestFile('/tmp', 'testdir/file1_20170101*')
         self.assert_result_matches_filename(filename)
 
     def test_noMatchingFiles(self):
+        print("Test findNewestFile no matching files:")
         filename = utils.findNewestFile('/tmp/testdir/', 'no_such_file')
         self.assertEqual(filename, '')
 
-    def assert_result_matches_filename(self, filename):
-        self.assertEqual(filename, TEST_BASE_FILENAME + str(NUM_TEST_FILES-1))
+
 
 
 #import matlab.engine
@@ -60,23 +67,31 @@ class TestMatlabStructDict(unittest.TestCase):
     def setUp(self):
         # matlab code to generate test file
         # self.matlab = matlab.engine.start_matlab()
-        # test.a = [1 2 3 4; 5 6 7 8; 9 10 11 12]
-        # test.a(:,:,2) = [13 14 15 16; 17 18 19 20; 21 22 23 24]
-        # test.b = 12
-        # c = 21
-        # d =  'hello'
-        # save('teststruct.mat', 'test', 'c', 'd')
+        # test.sub1 = [1 2 3 4; 5 6 7 8; 9 10 11 12]
+        # test.sub1(:,:,2) = [13 14 15 16; 17 18 19 20; 21 22 23 24]
+        # test.sub2 = 12
+        # top1 = 21
+        # top2 =  'hello'
+        # save('teststruct.mat', 'test', 'top1', 'top2')
         pass
     def tearDown(self):
         # self.matlab.quit()
         pass
     def test_loadStruct(self):
+        print("Test MatlabStructDict:")
         teststruct = sio.loadmat('teststruct.mat')
         test = utils.MatlabStructDict(teststruct, 'test')
-        self.assertEqual(test.b, 12)
-        self.assertEqual(test.c, 21)
-        self.assertEqual(test.d, 'hello')
+        self.assertEqual(test.sub2, 12)
+        self.assertEqual(test.top1, 21)
+        self.assertEqual(test.top2, 'hello')
         a = np.array([[[1, 13], [2, 14], [3, 15], [4, 16]],
                       [[5, 17], [6, 18], [7, 19], [8, 20]],
                       [[9, 21], [10, 22], [11, 23], [12, 24]]], dtype=np.uint8)
-        self.assertEqual(np.array_equal(test.a, a), True)
+        self.assertTrue(np.array_equal(test.sub1, a))
+        test.test.sub3 = np.array([1, 2, 3])
+        self.assertTrue(np.array_equal(test.sub3, np.array([1, 2, 3])))
+        test.top3 = np.array([[4, 5, 6], [7, 8, 9]])
+        self.assertTrue(np.array_equal(test.top3, np.array([[4, 5, 6], [7, 8, 9]])))
+
+if __name__ == "__main__":
+    unittest.main()
--- rtAttenPy/utils.py
+++ rtAttenPy/utils.py
@@ -7,6 +7,9 @@ import numpy as np
 
 
 def findNewestFile(filepath, filepattern):
+    '''Find newest file according to filesystem creation time
+        and return the filename.
+    '''
     full_path_pattern = ''
     if os.path.basename(filepattern) == filepath:
         # filepattern has the full path in it already
@@ -25,28 +28,80 @@ def findNewestFile(filepath, filepattern):
     except ValueError:
         return ''
 
+def convertStructuredArrayToDict(sArray):
+    '''Convert a NumPy structured array to a dictionary. Return the dictionary'''
+    rvDict = dict()
+    for key in sArray.dtype.names:
+        try:
+            val = sArray[key]
+            # Check for and flatten arrays with only one element in them
+            if isinstance(val, np.ndarray) and val.shape == (1, 1):
+                val = val[0][0]
+            rvDict[key] = val
+        except KeyError:
+            pass
+    return rvDict
+
 class MatlabStructDict(dict):
+    '''Subclass dictionary so that elements can be accessed either as dict['key']
+        of dict.key. If elements are of type NumPy structured arrays, convert
+        them to dictionaries and then to MatlabStructDict also.
+    '''
     def __init__(self, dictionary, name=None):
+        # name is used to identify a special field whose elements should be considered
+        #  as first level elements. i.e. name=patterns, then data.patterns.field
+        #  will return the same as data.field
         self.__name__ = name
         super().__init__(dictionary)
+        # For any numpy arrays that are structured arrays, convert the to MatlabStructDict
+        for key in self.keys():
+            try:
+                # structured arrays will have a non-zero set self[key].dtype.names
+                if isinstance(self[key], np.ndarray) and len(self[key].dtype.names) > 0:
+                    self[key] = MatlabStructDict(convertStructuredArrayToDict(self[key]))
+            except TypeError:
+                pass
 
     def __getattr__(self, key):
+        '''Implement get attribute so that for x=data.field can be used'''
         struct = self
+        # if the key isn't found at the top level, check if it is a sub-field of
+        # the special 'name' field
         if key not in self.keys() and self.__name__ in self.keys():
             struct = self[self.__name__]
         try:
             val = struct[key]
         except KeyError:
             val = None
+        # flatten numpy arrays
         while isinstance(val, np.ndarray) and val.shape == (1, 1):
             val = val[0][0]
         return val
 
     def __setattr__(self, key, val):
+        '''Implement set attribute for dictionary so that form data.field=x can be used'''
+        # check for special __fields__ and do default handling
         if re.match('__.*__', key):
             super().__setattr__(key, val)
             return
-        field_type = None
-        if isinstance(val, int) and val in range(256):
-            field_type = np.uint8
-        self[key] = np.array([[val]], dtype=field_type)
+        # pack ints in 2d array [[int]] as matlab does
+        if isinstance(val, int):
+            field_type = None
+            if val in range(256):
+                field_type = np.uint8
+            self[key] = np.array([[val]], dtype=field_type)
+        else:
+            self[key] = val
+
+    def fields(self):
+        '''list out all fields including the subfields of the special 'name' field'''
+        struct_fields = ()
+        try:
+            struct = self[self.__name__]
+            if isinstance(struct, np.ndarray):
+                struct_fields = struct.dtype.names
+        except KeyError:
+            pass
+        s = set().union(self.keys(), struct_fields)
+        # TODO - remove fields with __*__ pattern
+        return s
