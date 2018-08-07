9693e564c7bc5d986861d942bb1247e3317eb650
Daniel Suo <danielsuo@gmail.com>
2018-08-07 14:31:39 -0400

    Update environment.yml to add socat and update pyarrow to use ray's pyarrow

--- rtfMRI/Messaging.py
+++ rtfMRI/Messaging.py
@@ -6,7 +6,7 @@ import ssl
 import os
 import struct
 import logging
-import pyarrow as pa
+from ray import pyarrow as pa
 from .StructDict import StructDict
 from .MsgTypes import MsgType, MsgEvent, MsgResult
 from .Errors import MessageError, ValidationError
