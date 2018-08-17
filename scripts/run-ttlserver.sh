8f8bf849ef7d851a96531726218c2f8a54f302d8
Grant <gwallace@princeton.edu>
2018-08-17 13:18:04 -0400

    Simplify run-ttlserver script

--- scripts/run-ttlserver.sh
+++ scripts/run-ttlserver.sh
@@ -2,8 +2,6 @@
 source ~/.bashrc
 conda deactivate
 conda activate rtAtten
-chmod o+rw /dev/ttyACM0
-until python rtfMRI/ttlPulse.py -d /dev/ttyACM0; do
-    echo "Server crashed with exit code $?.  Respawning.." >&2
-    sleep 3
-done
+# chmod o+rw /dev/ttyACM0
+python rtfMRI/ttlPulse.py -d /dev/ttyACM0
+
